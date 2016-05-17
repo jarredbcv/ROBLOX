2356b1a47d88ecb2bc76f280c6e2ca55777acbd3
jarredbcv <jarredbcv@outlook.com>
2016-05-17 07:21:10 -0500

    Update assetdownload.php

--- assetdownload.php
+++ assetdownload.php
@@ -1,28 +1,12 @@
-<html><body>
 <?php
-	function get_site_html($site_url) 
-	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		curl_setopt($ch, CURLOPT_MAXREDIRS, 4);
-		curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-		curl_setopt($ch, CURLOPT_URL, $site_url);
-
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($ch);
-
-		global $base_url; 
-		$base_url = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-		$http_response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		curl_close ($ch);
-		return htmlentities($response);
-	}
-
-	echo get_site_html($_GET['url']);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 ?>
-</body>
-</html>
+
+<?php
+ $options  = array('http' => array('user_agent' => 'Roblox/WinInet'));
+ $context  = stream_context_create($options);
+ 
+ $xml = file_get_contents('http://www.roblox.com/asset/?id='.$_GET["Id"],false,$context);
+ echo htmlentities($xml);
+?>
